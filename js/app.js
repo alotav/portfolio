2f5011dd1ba94c323eacbfb27a8228bc7ae3f785
alotav <alonsotaverna@hotmail.com>
2023-07-24 16:52:10 -0300

    effect

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 // menu responsive:
 
 var btnOpen = document.getElementById("btnMenuOpen"),
-    btnClose = document.getElementById("btnMenuClose"),
-    navResponsive = document.getElementById("nav");
+  btnClose = document.getElementById("btnMenuClose"),
+  navResponsive = document.getElementById("nav");
 
 // Close click:
-btnClose.addEventListener("click", function(){
-    navResponsive.classList.remove("nav-active");
-    btnOpen.classList.remove("hide");
-})
+btnClose.addEventListener("click", function () {
+  navResponsive.classList.remove("nav-active");
+  btnOpen.classList.remove("hide");
+});
 
 // Open click:
-btnOpen.addEventListener("click", function(){
-    navResponsive.classList.add("nav-active");
-    btnOpen.classList.add("hide");
-})
+btnOpen.addEventListener("click", function () {
+  navResponsive.classList.add("nav-active");
+  btnOpen.classList.add("hide");
+});
 
 // Slider:
 
@@ -26,23 +26,51 @@ let totalImg = sliderImg.length;
 
 let sliderInnerDesc = document.querySelector(".description-inner");
 
-
 let time = 3000;
 let time2;
 let index = 0;
 
-setInterval(function(){
-    // Image
-    let porcentage = index * -100;
-    sliderInner.style.transform = `translateX(${porcentage}%)`;
-    // Description
-    let porcentage2 = index  * -50;
-    sliderInnerDesc.style.transform = `translateY(${porcentage2}vh)`;
+setInterval(function () {
+  // Image
+  let porcentage = index * -100;
+  sliderInner.style.transform = `translateX(${porcentage}%)`;
+  // Description
+  let porcentage2 = index * -50;
+  sliderInnerDesc.style.transform = `translateY(${porcentage2}vh)`;
 
+  index++;
 
-    index ++;
-
-    if(index >= totalImg){
-        index = 0;
-    }
+  if (index >= totalImg) {
+    index = 0;
+  }
 }, time);
+
+// Effect
+
+const effect = document.querySelector(".effect");
+const colors = ["#909090", "#c0b8b8", "#cdcdcd"];
+
+const figure = () => {
+  for (let i = 0; i <= 10; i++) {
+    let figure = document.createElement("span");
+    let select = Math.round(colors.length * Math.random());
+
+    figure.style.background =
+      colors[select > colors.length ? select - 1 : select];
+
+    //position box
+    figure.style.top = innerHeight * Math.random() + "px";
+    figure.style.left = innerWidth * Math.random() + "px";
+
+    effect.appendChild(figure);
+
+
+    setInterval(() => {
+        figure.style.top = innerHeight * Math.random() + "px";
+    figure.style.left = innerWidth * Math.random() + "px";
+    },7000);
+
+  }
+};
+
+figure();
